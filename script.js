e40ae7b6b9bd3ca4159fc7b03f701f0f45572ced
Matth-M <matthieu.matsima@gmail.com>
2022-08-16 03:31:19 +0200

    Add basic operations functions

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ numbers.forEach(number => {
 		resultContainer.textContent += this.textContent;	
 	});
 });
+function add(a,b){
+	return a + b;
+}
+
+function substract(a,b){
+	return a - b;
+}
+
+function multiply(a,b){
+	return a - b;
+}
+
+function divide(a,b){
+	return a / b;
+}
