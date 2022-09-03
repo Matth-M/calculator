40560bc4f3de66f9a80cdb8650fa59aec4967299
Matth-M <matthieu.matsima@gmail.com>
2022-09-04 01:00:38 +0200

    Refactor code calling the operators on click

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-let operator = '';
-let storedNb = 0;
+let context = {
+	operator: null,
+	storedNb: 0,
+}
+
+function setOperation(operator){
+	if(context.operator != null){
+		resultContainer.textContent = operate(operator, context.storedNb, +resultContainer.textContent);
+	}
+	context.operator = operator;
+	context.storedNb = +resultContainer.textContent;
+	clear();
+}
 
 const addBtn = document.getElementById('add');
-addBtn.addEventListener('click', function(){
-	operator = add;
-	storedNb = +resultContainer.textContent;
-	clear();
-});
+addBtn.addEventListener('click', setOperation(add));
 
 const subBtn = document.getElementById('substract');
-subBtn.addEventListener('click', function(){
-	operator = substract;
-	storedNb = +resultContainer.textContent;
-	clear();
-});
+subBtn.addEventListener('click', setOperation(substract));
 
 const mulBtn = document.getElementById('multiply');
-mulBtn.addEventListener('click', function(){
-	operator = multiply;
-	storedNb = +resultContainer.textContent;
-	clear();
-});
+mulBtn.addEventListener('click', setOperation(multiply));
 
 const divBtn = document.getElementById('divide');
-divBtn.addEventListener('click', function(){
-	operator = divide;
-	storedNb = +resultContainer.textContent;
-	clear();
-});
+divBtn.addEventListener('click', setOperation(divide));
 
 const equalBtn = document.getElementById('equal');
 equalBtn.addEventListener('click', function(){
